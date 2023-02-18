56f3115b9a3aa211e1586c135d717c9cdbe5b6e1
New <New@example.com>
2023-02-18 12:24:09 +0500

    Решение приера № 19

--- Exemple19/Program.cs
+++ Exemple19/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+int date = 0;
+int result = 0;
+int GetNumber(string message)
+{
+    Console.Write(message);
+    string value = Console.ReadLine();
+    int number = Convert.ToInt32(value);
+    return number;
+}
+
+int Palinom(int number)
+{
+    while(number > 0)
+    {
+        date = number % 10;
+        result = result * 10 + date;
+        number /= 10;
+         
+    }
+    return result;
+}
+
+void Print( int number, int result)
+{
+    if(number == result)
+    {
+    Console.WriteLine("Число " + number + "  полиндром");
+    }
+    else
+    {
+        Console.WriteLine("Число не является полиндромом");
+    }
+}
+
+int number = GetNumber("Введите число  ");
+Palinom(number);
+Print(number, result);
+
